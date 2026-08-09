cf87ee607692bf9e9d8bfa0a09b5cba3f5610f87
gagaprince <wangzidongprince@163.com>
2026-08-09 20:46:51 +0800

    改成ip可访问

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,6 +20,6 @@ async function bootstrap() {
   }
   app.useStaticAssets(resourcePath, { prefix: '/resourceFiles/' });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
+ server: {
+   host: true,
+   proxy: {
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
